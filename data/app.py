ca62c0f0c08c3ac09dec8c6169ef3673f335cf60
noitu <dimalego35@gmail.com>
2023-03-15 11:48:07 +0300

    change file structure

--- data/app.py
+++ data/app.py
@@ -2,4 +2,7 @@ class Application():
     def __init__(self):
         pass
 
+'''костыльный метод. нужно пофиксить чтобы каталог читался из базы'''
 def get_catalog():
+    catalog = {}
+    return catalog
