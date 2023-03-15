7f43ed792c1e7e520706d24780fbcff07f42ac91
noitu <dimalego35@gmail.com>
2023-03-15 12:14:33 +0300

    add new products pics; fix hot-products

--- data/app.py
+++ data/app.py
@@ -1,8 +1,22 @@
+from random import shuffle
+
+
 class Application():
     def __init__(self):
         pass
 
+
 '''костыльный метод. нужно пофиксить чтобы каталог читался из базы'''
+
+
 def get_catalog():
-    catalog = {}
+    prefix = './static/images/'
+    chair1 = ['Small lounge chair MD4', prefix + 'lounge-chair.jpg', 499]
+    chair2 = ['Gary Open-air chair', prefix + 'open-air-chair.jpg', 5499]
+    table1 = ['Open-air Coffee table', prefix + 'open-air-coffee-table.jpg', 8999]
+    puff = ['Pouf', prefix + 'pouf.jpg', 199]
+    blanket = ['Wool blanket №4', prefix + 'wool-blanket.jpg', 159]
+
+    catalog = [chair1, chair2, table1, puff, blanket]
+    shuffle(catalog )
     return catalog
--- views.py
+++ views.py
@@ -1,18 +1,19 @@
 from flask import render_template
 from flask import Flask
-from data.app import
+from data.app import get_catalog
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def base():
-    context = {}
-    return render_template('index.html', **context)
+    context = {'products': get_catalog()}
+    products = context
+    return render_template('index.html', **products)
 
 @app.route('/store')
 def store():
     return render_template('store.html')
 
 def main():
-    app.run()
+    app.run(debug=True)
