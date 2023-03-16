6b0bd13d2dc97ccdbf5d726a8578209633433750
noitu <dimalego35@gmail.com>
2023-03-16 22:27:49 +0300

    add new products to store

--- data/app.py
+++ data/app.py
@@ -30,6 +30,16 @@ def get_catalog():
     blanket = {'name': 'Wool blanket №4', 'image': prefix + 'wool-blanket.jpg', 'price': 159, 'link': '#'}
     sofa1 = {'name': 'Love sofa 2-seater', 'image': prefix + 'love-sofa.jpg', 'price': 6699, 'link': '#'}
 
-    catalog = [chair1, chair2, table1, puff, blanket, sofa1]
+    catalog = [chair1, chair2, table1, puff, blanket, sofa1,
+               {'name': 'Open-Air sofa table end (left or right)', 'image': prefix + 'vipp-720-open-air-sofa-open-end-left.jpg',
+                'price': 5500.00, 'link': '#'},
+               {'name': 'Lodge footstool curly', 'image': prefix + 'vipp-467-lodge-footstool-curly.jpg',
+                'price': 6800.00, 'link': '#'},
+               {'name': 'Dispenser', 'image': prefix + 'vipp-9-soap-dispenser-beige.jpg', 'price': 158.00,
+                'link': '#'},
+               {'name': 'Bath module, medium', 'image': prefix + 'vipp-982-bath-module_1.jpg', 'price': 6350.00,
+                'link': '#'},
+               {'name': 'Cabin round table, light oak base', 'image': prefix + 'vipp494_light-oak_jura_01-grey.jpg',
+                'price': 6250.00, 'link': '#'}]
     shuffle(catalog)
     return catalog
