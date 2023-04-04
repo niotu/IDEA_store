4dc56d36c9ee9f60a2567a3d50dab81b9b1ad7dd
noitu <dimalego35@gmail.com>
2023-04-04 20:31:47 +0300

    fix user, fix personal page

--- data/users.py
+++ data/users.py
@@ -13,11 +13,13 @@ class User(SqlAlchemyBase, UserMixin):
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String,
                               index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
+    address = sqlalchemy.Column(sqlalchemy.String)
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
---
+++ db/maketable.sql
@@ -0,0 +1,1 @@
+# here will be a script for db creation
