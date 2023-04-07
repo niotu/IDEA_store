2c7e9a8f0ee2edeaa558016cad8f4a067d7d27cb
SUPER-PAU <superpauks@gmail.com>
2023-04-07 19:14:45 +0300

    updated personal page

--- forms/user_form.py
+++ forms/user_form.py
@@ -1,15 +1,18 @@
 from wtforms import StringField
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, BooleanField, SubmitField, EmailField
+from wtforms import PasswordField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 
 class RegisterForm(FlaskForm):
-    name = StringField('username', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired()])
+    surname = StringField('surname', validators=[DataRequired()])
+    address = StringField('address')
     email = EmailField('email', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     password_again = PasswordField('password', validators=[DataRequired()])
     submit = SubmitField('register')
+
 
 
 class LoginForm(FlaskForm):
